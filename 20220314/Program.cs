35fad10e1519426a35c2873eadce3acb517ff8c3
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:16:46 +0800

    2

--- 20220314/Program.cs
+++ 20220314/Program.cs
@@ -10,6 +10,28 @@ namespace _20220314
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("請輸入一個數字");
+            string str1 = Console.ReadLine();
+
+            int num1 = Convert.ToInt32(str1);
+            if (num1 % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (num1 >= 2 && num1 <= 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (num1 >= 6 && num1 <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                Console.WriteLine("Not Weird");
+            }
+            Console.ReadKey();
         }
     }
 }
+
